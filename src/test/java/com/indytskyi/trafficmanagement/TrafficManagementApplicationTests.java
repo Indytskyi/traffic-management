853cd0f755e38d3d89a8182fd09c4912a923f6ef
Indytskyi Artem <ind.artem2003@gmail.com>
2023-03-16 16:07:39 +0200

    final commit

--- src/test/java/com/indytskyi/trafficmanagement/TrafficManagementApplicationTests.java
+++ src/test/java/com/indytskyi/trafficmanagement/TrafficManagementApplicationTests.java
@@ -1,15 +1,5 @@
 package com.indytskyi.trafficmanagement;
 
-import static java.lang.Math.asin;
-import static java.lang.Math.cos;
-import static java.lang.Math.pow;
-import static java.lang.Math.sin;
-import static java.lang.Math.sqrt;
-import static java.lang.Math.toRadians;
-
-import com.indytskyi.trafficmanagement.model.Airplane;
-import com.indytskyi.trafficmanagement.service.PlaneCalculation;
-import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
